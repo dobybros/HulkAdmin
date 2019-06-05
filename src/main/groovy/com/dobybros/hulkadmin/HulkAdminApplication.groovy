76575e6721274f1028662ce87570815f6c9c5829
Royal Chan <cn.royalchan@gmail.com>
2019-06-05 15:13:56 +0800

    init project

---
+++ src/main/groovy/com/dobybros/hulkadmin/HulkAdminApplication.groovy
@@ -0,0 +1,13 @@
+package com.dobybros.hulkadmin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HulkAdminApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HulkAdminApplication, args)
+	}
+
+}
