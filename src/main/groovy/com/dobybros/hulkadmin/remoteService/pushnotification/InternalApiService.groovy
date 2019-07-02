1550c7df883f65297250aea5dc94018d728c7c57
licklulu <lick1244036947@163.com>
2019-07-02 13:59:19 +0800

    pushnotification to pushnoti

--- src/main/groovy/com/dobybros/hulkadmin/remoteService/pushnotification/InternalApiService.groovy
+++ src/main/groovy/com/dobybros/hulkadmin/remoteService/pushnotification/InternalApiService.groovy
@@ -3,7 +3,7 @@ package com.dobybros.hulkadmin.remoteService.pushnotification
 import com.dobybros.hulkadmin.remoteService.pushnotification.AppInfo
 
 interface InternalApiService {
-     static final String SERVICE = "pushnotification"
+     static final String SERVICE = "pushnoti"
 
      void updateAllInfo();
 
