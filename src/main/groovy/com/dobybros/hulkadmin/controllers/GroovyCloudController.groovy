4dd00f6fd7bd6d68ef1fe6ac2e7382d38732c392
licklulu <lick1244036947@163.com>
2020-01-09 15:27:06 +0800

    modify serviceStubManager

--- src/main/groovy/com/dobybros/hulkadmin/controllers/GroovyCloudController.groovy
+++ src/main/groovy/com/dobybros/hulkadmin/controllers/GroovyCloudController.groovy
@@ -2,21 +2,12 @@ package com.dobybros.hulkadmin.controllers
 
 import chat.json.Result
 import com.alibaba.fastjson.JSON
-import com.dobybros.hulkadmin.remoteService.discovery.DiscoveryService
 import com.docker.data.DockerStatus
 import com.docker.data.Service
-import com.docker.rpc.remote.stub.ServiceStubManager
 import com.docker.storage.adapters.impl.DockerStatusServiceImpl
 import com.docker.utils.ScriptHttpUtils
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 import java.util.concurrent.ConcurrentHashMap
 
